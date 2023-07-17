4a9454b1d1a396bc7ab397f756ff57975603da4c
Varun033 <97447408+Varun033@users.noreply.github.com>
2023-07-17 11:52:43 +0530

    knapsack.c

---
+++ DSA ALL CODES/Graph/knapsack.c
@@ -0,0 +1,93 @@
+// C program for Dijkstra's single source shortest path
+// algorithm. The program is for adjacency matrix
+// representation of the graph
+
+#include <limits.h>
+#include <stdbool.h>
+#include <stdio.h>
+
+// Number of vertices in the graph
+#define V 6
+
+// A utility function to find the vertex with minimum
+// distance value, from the set of vertices not yet included
+// in shortest path tree
+int minDistance(int dist[], bool sptSet[], int con)
+{
+    // Initialize min value
+    int min = con, min_index;
+
+    for (int v = 0; v < V; v++)
+        if (sptSet[v] == false && dist[v] <= min)
+            min = dist[v], min_index = v;
+    return min_index;
+}
+
+// A utility function to print the constructed distance
+// array
+void printSolution(int dist[], int con)
+{
+    printf("Vertex \t\t Distance from Source\n");
+    for (int i = 0; i < V; i++)
+        if (dist[i] < con)
+        {
+            printf("%d \t\t\t\t \n", i);
+        }
+}
+
+// Function that implements Dijkstra's single source
+// shortest path algorithm for a graph represented using
+// adjacency matrix representation
+void dijkstra(int graph[V][V], int src, int con)
+{
+    int dist[V]; // The output array. dist[i] will hold the
+                 // shortest
+    // distance from src to i
+
+    bool sptSet[V];
+    for (int i = 0; i < V; i++)
+        dist[i] = con, sptSet[i] = false;
+
+    // Distance of source vertex from itself is always 0
+    dist[src] = 0;
+
+    // Find shortest path for all vertices
+    for (int count = 0; count < V - 1; count++)
+    {
+        int u = minDistance(dist, sptSet, con);
+
+        // Mark the picked vertex as processed
+        sptSet[u] = true;
+
+        for (int v = 0; v < V; v++)
+
+            if (!sptSet[v] && graph[u][v] && dist[u] != INT_MAX && dist[u] + graph[u][v] < dist[v])
+                dist[v] = dist[u] + graph[u][v];
+    }
+    printSolution(dist, con);
+}
+
+// driver's code
+int main()
+{
+    /* Let us create the example graph discussed above */
+    int graph[V][V];
+    int con;
+    printf("Enter cost\n");
+    scanf("%d", &con);
+    FILE *fp = fopen("input_dj.txt", "r");
+    for (int i = 0; i < V; i++)
+    {
+        for (int j = 0; j < V; j++)
+        {
+            fscanf(fp, "%d", &graph[i][j]);
+        }
+    }
+    fclose(fp);
+    int n;
+    printf("Enter the source vertex:\n");
+    scanf("%d", &n);
+    dijkstra(graph, n, con);
+
+    return 0;
+}
